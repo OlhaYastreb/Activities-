575b7e8c3d4c26d4ebcbe2778c89dc239d76af57
Olha Yastreb <olga_skyba@hotmail.com>
2021-03-13 14:08:20 -0500

    19.1

---
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,17 @@
+// Create a variable below
+var helloWorld = 'Hello World, my name is Olha';
+
+
+// Console.log() your variable here.
+console.log(helloWorld);
+alert(helloWorld);
+alert("yes I'm here");
+
+var x = 3;
+var y = 2;
+var z = 'there are ' + (x + y) + ' kids at my house';
+
+function addkids() {
+    console.log(z);
+}
+addkids();
